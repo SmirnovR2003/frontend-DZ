f0c6d7871b42fb6897994bc72b0199d04905506e
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2022-04-09 21:58:43 +0300

    maked frontend dz

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+export interface Task{
+  id: number,
+  name: string
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+ 
+  tasks: Array<Task> = [{
+    id: 1, 
+    name: "Go to cinema"}
+  ]
+
+  completedTasks: Array<Task> = []
+
+  taskId : number = 2
+  addTask(myForm: NgForm): void{
+    this.tasks.push({
+      id: this.taskId,
+      name: myForm.value.task});
+    this.taskId++;  
+  }
+
+  onDelete(id: number): void{
+    this.tasks = this.tasks.filter(task => task.id != id)
+  }
+
+  onDeleteCompletedTask(id: number): void{
+    this.completedTasks = this.completedTasks.filter(task => task.id != id)
+  }
+
+  onComplete(id: number): void{
+    let completedIndex = this.tasks.findIndex(task => task.id == id); 
+    this.completedTasks.push(this.tasks[completedIndex]);
+    this.tasks = this.tasks.filter(task => task.id != id);
+  }
+}
